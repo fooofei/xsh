840febc36bb7937e1be8d98e71a8f7b4762713ee
xied5531 <xied5531@qq.com>
2020-01-25 23:18:33 +0800

    init

--- xsh-prompt.go
+++ xsh-prompt.go
@@ -285,3 +285,33 @@ func reload() {
 	InitXAuth()
 	InitXHost()
 }
+
+func help() {
+	fmt.Println(`NAME:
+   xsh - A simple command line tool to run commands on remote host addresses.
+
+SYNOPSIS:
+   [KEYWORD]... [ARG]... 
+
+VERSION:
+` + Version + `
+
+DESCRIPTION:
+   Please report a issue at ` + XConfig.IssueUrl + ` if possible.
+
+   :help or :h                         Show help info
+   :set [group=xxx|address=x.x.x.x]    Load environment
+   :show                               Show address list of current group
+
+   :do                                 Run ssh command as normal user, default.
+   :sudo                               Run ssh command as su user from normal user
+   :copy                               Upload or download file or directory between local and remote
+       local -> remote                 -> means upload, remote must be directory
+       local <- remote                 <- means download, local must be directory
+
+   :encrypt passwd                     Encrypt passwd
+   :decrypt passwd                     Decrypt passwd
+
+   :exit or :quit or :q                Stop xsh
+`)
+}

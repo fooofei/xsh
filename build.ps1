ebde87f18f4209dc12171cc42790f6880befaa2d
xied5531 <xied5531@qq.com>
2020-01-25 23:18:33 +0800

    init

--- build.ps1
+++ build.ps1
@@ -1,16 +1,16 @@
 ﻿$env:GOOS = "linux"
 $env:GOARCH = "386"
-go build -o bin\xsh-linux-386
+go build -o release\xsh-linux-386
 
 $env:GOARCH = "amd64"
-go build -o bin\xsh-linux-amd64
+go build -o release\xsh-linux-amd64
 
 $env:GOARCH = "arm64"
-go build -o bin\xsh-linux-arm64
+go build -o release\xsh-linux-arm64
 
 $env:GOOS = "windows"
 $env:GOARCH = "386"
-go build -o bin\xsh-windows-386.exe
+go build -o release\xsh-windows-386.exe
 
 $env:GOARCH = "amd64"
-go build -o bin\xsh-windows-amd64.exe
+go build -o release\xsh-windows-amd64.exe
